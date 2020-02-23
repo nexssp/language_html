240da0a31815d7d8d9fa8e3daf49f792bbcba4c1
Marcin Polak <mapoart@gmail.com>
2020-02-23 16:30:13 +0100

    Added url

--- html.win32.nexss.config.js
+++ html.win32.nexss.config.js
@@ -2,7 +2,7 @@ let languageConfig = Object.assign({}, require("../config.win32"));
 languageConfig.title = "HTML";
 languageConfig.description =
   "Hypertext Markup Language (HTML) is the standard markup language for documents designed to be displayed in a web browser";
-languageConfig.url = "";
+languageConfig.url = "https://www.w3.org/html";
 languageConfig.extensions = [".html"];
 languageConfig.builders = {};
 languageConfig.compilers = {
