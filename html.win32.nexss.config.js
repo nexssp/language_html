a6191c8c5e759dbb34d257c048eb92318098bb83
Marcin Polak <mapoart@gmail.com>
2020-03-05 10:41:37 +0100

    Added authors, developers, year..

--- html.win32.nexss.config.js
+++ html.win32.nexss.config.js
@@ -3,7 +3,10 @@ languageConfig.title = "HTML";
 languageConfig.description =
   "Hypertext Markup Language (HTML) is the standard markup language for documents designed to be displayed in a web browser";
 languageConfig.url = "https://www.w3.org/html";
-languageConfig.extensions = [".html"];
+languageConfig.founders = ["Berners-Lee", "Dan Connolly"];
+languageConfig.developers = [""];
+languageConfig.years = ["1991"];
+languageConfig.extensions = [".html", ".htm"];
 languageConfig.builders = {};
 languageConfig.compilers = {
   html: {
